5b6f8d13aff776f7fbfbb5f06376de76e006d751
yusei-wy <yusei.kasa@gmail.com>
2025-03-29 21:00:50 +0900

    add: abc085 c

--- abc085/c/Main.hs
+++ abc085/c/Main.hs
@@ -1,9 +1,20 @@
 module Main where
 
+import Data.List (find)
+import Data.Maybe (fromMaybe, listToMaybe)
+import System.FilePath (isValid)
+
+data ThreeInts = ThreeInts Int Int Int
+instance Show ThreeInts where
+    show (ThreeInts a b c) = unwords [show a, show b, show c]
+
 main :: IO ()
 main = do
-    s <- getLine
-    print $ solve s
+    [n, y] <- map read . words <$> getLine :: IO [Int]
+    print $ solve n y
 
-solve :: String -> Int
-solve s = undefined
+solve :: Int -> Int -> ThreeInts
+solve n y = fromMaybe (ThreeInts (-1) (-1) (-1)) $ find isValid candidates
+  where
+    candidates = [ThreeInts a b (n - a - b) | a <- [0 .. n], b <- [0 .. n - a]]
+    isValid (ThreeInts a b c) = 10000 * a + 5000 * b + 1000 * c == y
