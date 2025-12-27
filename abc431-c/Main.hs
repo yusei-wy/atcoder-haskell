440ccea1e2dba7fea84fac484683f4678a39a263
yusei-wy email = 31252054+yusei-wy@users.noreply.github.com <yusei.kasa@gmail.com>
2025-12-28 00:13:39 +0900

    refactor: ByteStringによる入出力最適化と末尾再帰によるマッチング処理の改善

--- abc431-c/Main.hs
+++ abc431-c/Main.hs
@@ -1,28 +1,39 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Main where
 
+import Data.ByteString.Char8 qualified as BS
 import Data.List (sort)
 import Data.List qualified (sortBy)
+import Data.Maybe (fromJust)
+
+readInts :: IO [Int]
+readInts = do
+  line <- BS.getLine
+  return $ map (fst . fromJust . BS.readInt) (BS.words line)
 
 main :: IO ()
 main = do
-  [n, m, k] <- map read . words <$> getLine :: IO [Int]
-  heads <- sort . map read . words <$> getLine :: IO [Int]
-  bodies <- sort . map read . words <$> getLine :: IO [Int]
+  [n, m, k] <- readInts
+  heads <- readInts
+  bodies <- readInts
 
-  if match heads bodies >= k
+  if match (sort heads) (sort bodies) >= k
     then
-      putStr "Yes"
+      BS.putStrLn $ BS.pack "Yes"
     else
-      putStr "No"
+      BS.putStrLn $ BS.pack "No"
 
 -- sort 済みの2つの配列の要素を1つずつ比較し
 -- b が a 以上なら +1 をカウントして、最終的な結果を返す
 match :: [Int] -> [Int] -> Int
-match [] _ = 0
-match _ [] = 0
-match (h : hs) (b : bs)
-  -- 一致するなら両方消費できる
-  | h <= b = 1 + match hs bs
-  -- 一致しないなら R だけつまり bodies だけを進めることで
-  -- O(N) の計算量で無駄なく比較できる
-  | otherwise = match (h : hs) bs
+match hs bs = go 0 hs bs
+ where
+  go !cnt [] _ = cnt
+  go !cnt _ [] = cnt
+  go !cnt (h : hs) (b : bs)
+    -- 一致するなら両方消費できる
+    | h <= b = go (cnt + 1) hs bs
+    -- 一致しないなら R だけつまり bodies だけを進めることで
+    -- O(N) の計算量で無駄なく比較できる
+    | otherwise = go cnt (h : hs) bs
